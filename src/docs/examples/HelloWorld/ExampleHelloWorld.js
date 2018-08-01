02570927626ff1b9e7ea432bb08674163761daee
arashfarmani <arash_farmani@msn.com>
2018-08-01 16:14:35 +0200

    docs finished

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import HelloWorld from 'react-comp/HelloWorld'
+
+/** Custom message */
+export default function ExampleHelloWorld(){
+    return <HelloWorld message="people" />
+}
