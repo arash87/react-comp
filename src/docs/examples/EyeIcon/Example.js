468551543896fed3de1eea6320dabeac4df05a4b
arashfarmani <arash_farmani@msn.com>
2018-08-01 20:18:39 +0200

    atoms (progressbar, label and icon ) added

---
+++ src/docs/examples/EyeIcon/Example.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import EyeIcon from 'react-comp/EyeIcon'
+
+export default function EyeIconExample(){
+    return <EyeIcon />
+}
---
+++ src/docs/examples/ProgressBar/Example70Percent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ProgressBar from 'react-comp/ProgressBar'
+
+/** 70% progress */
+export default function Example20Percent(){
+    return <ProgressBar percent={70} width={150} />
+}
