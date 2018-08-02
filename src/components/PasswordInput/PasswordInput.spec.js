4c715c8e6df57befcd9b9e720effa333f19452f5
arashfarmani <arash_farmani@msn.com>
2018-08-02 17:35:12 +0200

    interaction test

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import PasswordInput from './PasswordInput'
+import {shallow} from 'enzyme'
 
 test('hides password quality by default', () => {
     const tree = renderer.create(<PasswordInput 
@@ -10,4 +11,26 @@ test('hides password quality by default', () => {
         value="blabla" />).toJSON()
     
     expect(tree).toMatchSnapshot()
+})
+
+test('toggles input type when show/hide password clicked', () => {
+    const wrapper = shallow(<PasswordInput 
+        htmlId="test"
+        name="test"
+        value=""
+        onChange={() => {}}
+        showVisibilityToggle
+        />
+    )
+
+    // Enzyme uses in-memory DOM here
+    // Passwordinput should have a type of password initially  
+    expect(wrapper.find({type: 'password'})).toHaveLength(1)
+    expect(wrapper.find({type: 'text'})).toHaveLength(0)
+
+    wrapper.find('a').simulate('click')
+    
+    expect(wrapper.find({type: 'password'})).toHaveLength(0)
+    expect(wrapper.find({type: 'text'})).toHaveLength(1)
+
 })
